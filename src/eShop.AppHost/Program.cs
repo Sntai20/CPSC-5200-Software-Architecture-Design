e89d6cd75d8691fda11bf613be28ed2c0d188c3b
Antonio Santana <ansantan@microsoft.com>
2024-05-25 14:42:35 -0700

    `Program.cs` updated with method additions, object modifications, and namespace removal
    Removed `Microsoft.Extensions.Configuration` namespace and OpenAI related code from `Program.cs`. Added `AddForwardedHeaders()` method to `builder` object and modified `webApp` object to include a project with a launch profile name and references to `catalogApi`, `orderingApi`, `rabbitMq`. Also set `IdentityUrl` environment variable. Configured callback URLs for `webApp` and `webhooksClient` using `WithEnvironment` method.

--- src/eShop.AppHost/Program.cs
+++ src/eShop.AppHost/Program.cs
@@ -1,5 +1,4 @@
 ﻿using eShop.AppHost;
-using Microsoft.Extensions.Configuration;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
@@ -65,46 +64,6 @@ var webApp = builder.AddProject<Projects.WebApp>("webapp", launchProfileName)
     .WithReference(orderingApi)
     .WithReference(rabbitMq)
     .WithEnvironment("IdentityUrl", identityEndpoint);
-
-// set to true if you want to use OpenAI
-bool useOpenAI = false;
-if (useOpenAI)
-{
-    const string openAIName = "openai";
-    const string textEmbeddingName = "text-embedding-ada-002";
-    const string chatModelName = "gpt-35-turbo-16k";
-
-    // to use an existing OpenAI resource, add the following to the AppHost user secrets:
-    // "ConnectionStrings": {
-    //   "openai": "Key=<API Key>" (to use https://api.openai.com/)
-    //     -or-
-    //   "openai": "Endpoint=https://<name>.openai.azure.com/" (to use Azure OpenAI)
-    // }
-    IResourceBuilder<IResourceWithConnectionString> openAI;
-    if (builder.Configuration.GetConnectionString(openAIName) is not null)
-    {
-        openAI = builder.AddConnectionString(openAIName);
-    }
-    else
-    {
-        // to use Azure provisioning, add the following to the AppHost user secrets:
-        // "Azure": {
-        //   "SubscriptionId": "<your subscription ID>"
-        //   "Location": "<location>"
-        // }
-        openAI = builder.AddAzureOpenAI(openAIName)
-            .AddDeployment(new AzureOpenAIDeployment(chatModelName, "gpt-35-turbo", "0613"))
-            .AddDeployment(new AzureOpenAIDeployment(textEmbeddingName, "text-embedding-ada-002", "2"));
-    }
-
-    catalogApi
-        .WithReference(openAI)
-        .WithEnvironment("AI__OPENAI__EMBEDDINGNAME", textEmbeddingName);
-
-    webApp
-        .WithReference(openAI)
-        .WithEnvironment("AI__OPENAI__CHATMODEL", chatModelName); ;
-}
 
 // Wire up the callback urls (self referencing)
 webApp.WithEnvironment("CallBackUrl", webApp.GetEndpoint(launchProfileName));
